4882d7fe89046e98de1915266a2a236cf08ff26c
Navid <Navid_mon@yahoo.com>
2021-01-12 23:39:27 +0330

    SBS-13 Create react project, add call method in App.js SBS-14 Solve CROS and add "shiningbeautysalon.com" to appetting in api project

--- ShiningBeautySalon.API/Startup.cs
+++ ShiningBeautySalon.API/Startup.cs
@@ -17,6 +17,7 @@ namespace ShiningBeautySalon.API
 {
     public class Startup
     {
+        private readonly string AllowedOrigins = "_Origins"; 
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -27,6 +28,18 @@ namespace ShiningBeautySalon.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(AllowedOrigins, builder =>
+                {
+                    builder
+                        .WithOrigins(Configuration.GetSection("AllowOrigin").Value.Split(";"))
+                        .SetIsOriginAllowedToAllowWildcardSubdomains()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+            });
             services.AddControllers();
 
             services.AddDbContext<ShiningContext>(options =>
@@ -40,13 +53,13 @@ namespace ShiningBeautySalon.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCors(AllowedOrigins);
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
+            //app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
---
+++ ShiningBeautySalon.WebReact/clinetapp/src/utilize/http.jsx
@@ -0,0 +1,46 @@
+﻿import Axios from 'axios'
+ 
+function parseBody(response) {
+    if (response.status === 200) {
+        return response;
+    } else {
+        console.warn('Error parseBody', response);
+        return Promise.reject(response);
+    }
+}
+
+let instance = Axios.create({
+    withCredentials: true,
+    timeout: 60000,
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Credentials': 'true',
+    }
+});
+
+instance.interceptors.request.use((config) => {
+    config.headers.apiKey = "5E5AE551-D93E-4801-BC02-22A5CEB71F08";
+    return config;
+}, error => {
+    console.log(error);
+    return Promise.reject(error)
+});
+
+instance.interceptors.response.use((response) => {
+    return parseBody(response)
+}, error => {
+    if (error.response == undefined) {
+        return Promise.reject(error);
+    }
+
+    console.warn('Error status', error.response.status);
+
+    if (error.response.status == 401) {
+        localStorage.removeItem('userIdentity');
+        window.location.href = "/login";
+    }
+    return Promise.reject(error);
+});
+
+export const http = instance
