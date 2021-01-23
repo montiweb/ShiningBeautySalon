8bb66245fd79aa2b69924e7dbbf21c25d57ee319
Moein Montazeripour <moein@montiwebsolutions.com>
2021-01-23 18:39:20 -0500

    SBS-16 Added an Angular project to solution as a website

---
+++ ShiningBeautySalon-WebAngular/src/app/salon.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SalonService {
+
+  constructor(private http: HttpClient) { }
+
+  get(): Observable<any[]> {
+    //return this.http.get<any[]>(`http://api.shiningbeautysalon.com/Salon`);
+    return this.http.get<any[]>(`https://localhost:44355/Salon`);
+  }
+}
