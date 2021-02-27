3d99bd0ae7d9f810ca69561d6b0fd849cc765d51
Moein Montazeripour <moein@montiwebsolutions.com>
2021-02-26 19:56:59 -0500

    SBS-39 In Progress

---
+++ ShiningBeautySalon.API/Controllers/ServiceItemController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+
+using ShiningBeautySalon.API.Interfaces;
+using ShiningBeautySalon.Domain.Entities;
+using ShiningBeautySalon.Service.Interfaces;
+
+namespace ShiningBeautySalon.API.Controllers
+{
+    [ApiController]
+    [Route("[ServiceItem/[action]]")]
+    public class ServiceItemController : ControllerBase, IBaseController<ServiceItem>
+    {
+        private readonly IServiceItemService _serviceItemService;
+        public ServiceItemController(IServiceItemService serviceItemService)
+        {
+            _serviceItemService = serviceItemService;
+        }
+        public IActionResult GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult GetByID(int ID)
+        {
+            throw new NotImplementedException();
+        }
+        public IActionResult Add(ServiceItem model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Delete(ServiceItem model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult Update(ServiceItem model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ShiningBeautySalon.Service/Interfaces/IServiceItemService.cs
@@ -0,0 +1,11 @@
+﻿using ShiningBeautySalon.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShiningBeautySalon.Service.Interfaces
+{
+    public interface IServiceItemService: IBaseService<ServiceItem>
+    {
+    }
+}
