275e38ade08e847cf48dce0dafc17cd5e2e95b6d
Moein Montazeripour <moein@montiwebsolutions.com>
2021-02-26 20:02:59 -0500

    SBS-41 In Progress

---
+++ ShiningBeautySalon.API/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using ShiningBeautySalon.API.Interfaces;
+using ShiningBeautySalon.Domain.Entities;
+using ShiningBeautySalon.Service.Interfaces;
+
+namespace ShiningBeautySalon.API.Controllers
+{
+    [ApiController]
+    [Route("[User/[action]]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public IActionResult GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IActionResult GetByID(int ID)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IActionResult Add(User model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IActionResult Delete(User model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IActionResult Update(User model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
