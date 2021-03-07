813b0c15fd0e906dd096fcb171ec2e28478eff09
Moein Montazeripour <moein@montiwebsolutions.com>
2021-03-07 11:41:04 -0500

    SBS-43 In Progress

--- ShiningBeautySalon.API/Controllers/WeatherForecastController.cs
+++ ShiningBeautySalon.API/Controllers/WeatherForecastController.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -16,24 +16,109 @@ namespace ShiningBeautySalon.API.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        // // 1- IEnumerable
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+        //// 2- IActionResult by default JSON format
+        //[HttpGet]
+        //public IActionResult Get()
+        //{
+        //    var rng = new Random();
+        //    var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
 
+        //    return new JsonResult(result);
+        //}
+
+        ////  3- IActionResult by Altering the Response Status Code
+        // [HttpGet]
+        // public IActionResult Get()
+        // {
+        //     var rng = new Random();
+        //     var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //     {
+        //         Date = DateTime.Now.AddDays(index),
+        //         TemperatureC = rng.Next(-20, 55),
+        //         Summary = Summaries[rng.Next(Summaries.Length)]
+        //     })
+        //     .ToArray();
+
+        //     Response.StatusCode = StatusCodes.Status400BadRequest;
+
+        //     return new JsonResult(result);
+        // }
+
+
+        ////  4- IActionResult by helper methods on the ControllerBase object
+        //[HttpGet]
+        //public IActionResult Get()
+        //{
+        //    var rng = new Random();
+        //    var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+
+        //    Response.StatusCode = StatusCodes.Status400BadRequest;
+
+        //    return Ok(result);
+        //}
+
+        ////  5- IActionResult by return StatusCode()
+        //[HttpGet]
+        //public IActionResult Get()
+        //{
+        //    var rng = new Random();
+        //    var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+
+        //    Response.StatusCode = StatusCodes.Status400BadRequest;
+
+        //    return StatusCode(StatusCodes.Status500InternalServerError, result);
+        //}
+
+        // 6- IActionResult by return StatusCode(new JsonResult)
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IActionResult Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+
+            Response.StatusCode = StatusCodes.Status400BadRequest;
+
+            return StatusCode(StatusCodes.Status500InternalServerError,new JsonResult(result));
         }
+
+
     }
 }
