fed176d67a8f184c42c1be82b96dc4701a8f837e
Moein Montazeripour <moein@montiwebsolutions.com>
2021-03-06 21:09:22 -0500

    SBS-43 In Progress

--- ShiningBeautySalon.API/Controllers/LevelController.cs
+++ ShiningBeautySalon.API/Controllers/LevelController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
 using ShiningBeautySalon.Domain.Entities;
 using ShiningBeautySalon.Service.Interfaces; 
 
@@ -14,6 +16,25 @@ namespace ShiningBeautySalon.API.Controllers
             _levelService = levelService;
         }
 
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_levelService.GetAll());
+
+        [HttpGet]
+        public IActionResult GetByID(int levelID) {
+
+            try
+            {
+                if (!(levelID > 0))
+                    return StatusCode(StatusCodes.Status400BadRequest, "The Level ID can not be empty");
+
+                return StatusCode(StatusCodes.Status200OK, _levelService.GetByID(levelID));
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
         [HttpPost]
         public IActionResult Add(Level model) => Ok(_levelService.Add(model));
 
@@ -22,11 +43,5 @@ namespace ShiningBeautySalon.API.Controllers
 
         [HttpPost]
         public IActionResult Delete(Level model) => Ok(_levelService.Delete(model));
-
-        [HttpGet]
-        public IActionResult GetAll() => Ok(_levelService.GetAll());
-
-        [HttpGet]
-        public IActionResult GetByID(int levelId) => Ok(_levelService.GetByID(levelId));
     }
 }
--- ShiningBeautySalon.API/Startup.cs
+++ ShiningBeautySalon.API/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using ShiningBeautySalon.DependencyResolver.Ioc;
 
 namespace ShiningBeautySalon.API
