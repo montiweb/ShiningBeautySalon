72c9bed6dc909dc19a3037c712c4087d8d454063
Moein Montazeripour <moein@montiwebsolutions.com>
2021-03-30 21:38:36 -0400

    SBS-43

--- ShiningBeautySalon.API/Startup.cs
+++ ShiningBeautySalon.API/Startup.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 using ShiningBeautySalon.DependencyResolver.Ioc;
 
@@ -46,6 +46,7 @@ namespace ShiningBeautySalon.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
             app.UseCors(AllowedOrigins);
 
             app.UseHttpsRedirection();
